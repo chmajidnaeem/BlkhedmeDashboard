0458b29ea704b746f7fb109b50063995489deda9
Ariba Memon <76126161+Ariba-memon@users.noreply.github.com>
2024-09-29 23:43:30 +0500

    newcode

--- src/redux/api/locationAPI.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'https://apiv2.blkhedme.com/api/admin/locations';
-
-export const fetchLocations = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
-};
